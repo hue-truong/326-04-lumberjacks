2d8280bb98c99d3c1309b629dd2e0a5c11975768
Hue Truong <httruong@umass.edu>
2022-04-20 19:39:12 -0400

    added test picks, fixed broken refs

--- backend.js
+++ backend.js
@@ -4,14 +4,15 @@ import * as http from 'http';
 import * as url from 'url';
 import pkg from 'pg';
 const { Pool, Client } = pkg;
+import { faker } from '@faker-js/faker';
 
-const client = new Client({ 
-    "user": "postgres",
-    "host": "localhost",
-    "database": "postgres",
-    "password": "123",
-    "port": 5432
-});
+// const client = new Client({ 
+//     "user": "postgres",
+//     "host": "localhost",
+//     "database": "postgres",
+//     "password": "123",
+//     "port": 5432
+// });
 
 const app = express();
 const port = 3000;
@@ -22,43 +23,55 @@ app.use(express.urlencoded({ extended: false }));
 // Query jobs from specific company in Jobs table
 app.get('/companies/company/get-jobs', async (req, r) => {
     const COMMAND = `SELECT * FROM jobs WHERE company = ${req.query.company}`
-    client.connect()
+    client.connect();
     client.query(COMMAND, (err, res) => {
-        if(err){ r.status(501).send("ERROR: Could not get company!"); }
-        else{ r.status(200).send(res); }
-    })
+        if (err) { r.status(501).send("ERROR: Could not get jobs!"); }
+        else { r.status(200).send(res); }
+    });
+    client.end();
 })
 
 // Query information on company from Companies table
 app.get('/companies/get-company', async (req, r) => {
-    const COMMAND = `SELECT * FROM users WHERE id = ${req.query.id}`
-    client.connect()
+    const COMMAND = `SELECT * FROM companies WHERE id = ${req.query.id}`
+    client.connect();
     client.query(COMMAND, (err, res) => {
-        if(err){ r.status(501).send("ERROR: Could not get company!"); }
-        else{ r.status(200).send(res); }
+        if (err) { r.status(501).send("ERROR: Could not get company!"); }
+        else { r.status(200).send(res); }
     })
+    client.end();
 })
 
 // Query user information from Users table
 app.get('/users/get-user', async (req, r) => {
     const COMMAND = `SELECT * FROM users WHERE id = ${req.query.id}`
-    client.connect()
+    client.connect();
     client.query(COMMAND, (err, res) => {
-        if(err){ r.status(501).send("ERROR: Could not get user!"); }
-        else{ r.status(200).send(res); }
+        if (err) { r.status(501).send("ERROR: Could not get user!"); }
+        else { r.status(200).send(res); }
     })
+    client.end();
 })
 
 // Query random top picks for home page
 app.get('/companies/get-top-picks', async (req, r) => {
-    const COMMAND = 'SELECT id FROM companies ORDER BY RANDOM() LIMIT 5'
-    client.connect()
-    client.query(COMMAND, (err, res) => {
-        if(err){ r.status(501).send("ERROR: Could not get top picks!"); }
-        else{ r.status(200).send(res); }
+    // const COMMAND = 'SELECT id FROM companies ORDER BY RANDOM() LIMIT 5'
+    // client.connect();
+    // client.query(COMMAND, (err, res) => {
+    //     if(err){ r.status(501).send("ERROR: Could not get top picks!"); }
+    //     else{ r.status(200).send(res); }
+    // });
+    // client.end();
+    const test = new Array(5).fill().map(x => {
+        return {
+            name: faker.company.companyName(),
+            img: faker.image.cats(512, 512, true)
+        }
     })
+
+    r.send(test)
 })
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
-  });
+});
