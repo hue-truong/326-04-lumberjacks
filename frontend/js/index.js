e7820e15e74587da4bbbf14ecb33d46df20d4594
Berke <mgureltol@umass.edu>
2022-04-20 19:19:37 -0400

    index.html and index.js edited

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,9 +1,44 @@
-const buttons = document.getElementsByClassName("button2");
+const { faker } = require('@faker-js/faker');
 
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', callToServer); //false
+const topPick = document.getElementById("topPick");
+const trendingCompanies = document.getElementById("trendingCompanies");
+
+const tpResponse = await fetch(`/get-top-picks`, {
+    method: 'GET',
+  });
+  const tcResponse = await fetch(`/get-top-picks`, {
+    method: 'GET',
+  });
+
+
+for(let i = 0; i<5; ++i){
+
+    //fetch top-picks
+    const image1 = tpResponse[i];
+    const data1 = await tpResponse.json();
+    const tpDiv = document.createElement(`<img><a href=${image1}><img>`);
+    topPick.appendChild(tpDiv);
+
+      
+    //fetch trending-companies
+    
+    const image2 = tcResponse[i];
+    const data2 = await tcResponse.json();
+    const tcDiv = document.createElement(`<img><a href=${image2}><img>`);
+    trendingCompanies.appendChild(tcDiv);
+
 }
 
-function callToServer(){
-    console.log("This will be a call to server");
-}
+// button
+
+// const buttons = document.getElementsByClassName("button2");
+
+
+// for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener('click', callToServer); //false
+// }
+
+// function callToServer(){
+//     console.log("This will be a call to server");
+// }
+
