5756fb1fefacd9debd29178983e218ee185a3279
Berke <mgureltol@umass.edu>
2022-04-20 22:18:15 -0400

    changed jobs.js to make it work with prev. css

--- frontend/js/jobs.js
+++ frontend/js/jobs.js
@@ -50,22 +50,50 @@ for(let i = 0; i<5; ++i){
     //technology
     
     const technologyData = technologyArr[i];
-    technologyGrid.innerHTML = `<img src="${technologyData.img}" alt="${technologyData.name}"><img>`;
+    const techDiv = document.createElement("img");
+    const tempDiv1 = document.createElement("div");
+    tempDiv1.setAttribute("class", "grid-item");
+    techDiv.setAttribute("src", technologyData.img);
+    techDiv.setAttribute("alt", technologyData.name);
+    techDiv.setAttribute("class", "img");
+    tempDiv1.appendChild(techDiv);
+    technologyGrid.appendChild(tempDiv1);
 
       
     //architecture
     
     const architectureData = architectureArr[i];
-    architectureGrid.innerHTML = `<img src="${architectureData.img}" alt="${architectureData.name}"><img>`;
+    const tempDiv2 = document.createElement("div");
+    tempDiv2.setAttribute("class", "grid-item");
+    const archDiv = document.createElement("img");
+    archDiv.setAttribute("src", architectureData.img);
+    archDiv.setAttribute("alt", architectureData.name);
+    archDiv.setAttribute("class", "img");
+    tempDiv2.appendChild(archDiv);
+    architectureGrid.appendChild(tempDiv2);
 
     //data_analysis
     
     const data_analysisData = data_analysisArr[i];
-    data_analysisGrid.innerHTML = `<img src="${data_analysisData.img}" alt="${data_analysisData.name}"><img>`;
+    const tempDiv3 = document.createElement("div");
+    tempDiv3.setAttribute("class", "grid-item");
+    const data_analysisDiv = document.createElement("img");
+    data_analysisDiv.setAttribute("src", data_analysisData.img);
+    data_analysisDiv.setAttribute("alt", data_analysisData.name);
+    data_analysisDiv.setAttribute("class", "img");
+    tempDiv3.appendChild(data_analysisDiv);
+    data_analysisGrid.appendChild(tempDiv3);
 
     //entertainment
     const entertainmentData = entertainmentArr[i];
-    entertainmentGrid.innerHTML = `<img src="${entertainmentData.img}" alt="${entertainmentData.name}"><img>`;
+    const tempDiv4 = document.createElement("div");
+    tempDiv4.setAttribute("class", "grid-item");
+    const entertainmentDiv = document.createElement("img");
+    entertainmentDiv.setAttribute("src", entertainmentData.img);
+    entertainmentDiv.setAttribute("alt", entertainmentData.name);
+    entertainmentDiv.setAttribute("class", "img");
+    tempDiv4.appendChild(entertainmentDiv);
+    entertainmentGrid.appendChild(tempDiv4);
 }
 
 
