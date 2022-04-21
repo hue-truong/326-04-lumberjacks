d184010c80dbc02cc525071686cfe10323392abd
Hue Truong <httruong@umass.edu>
2022-04-20 23:16:27 -0400

    added endpoint to serve jobs and companies page

--- backend.js
+++ backend.js
@@ -109,20 +109,20 @@ app.get('/companies/get-trending-companies', jsonParser, async (req, r) => {
     r.status(200).send(test)
 })
 
-// app.get('/companies/get-jobs', jsonParser, async (req, r) => {
+app.get('/companies/get-companies', jsonParser, async (req, r) => {
     
-//     const test = new Array(5).fill().map(x => {
-//         return {
-//             name: faker.company.companyName(),
-//             img: faker.image.cats(512, 512, true)
-//         }
-//     })
+    const test = new Array(5).fill().map(x => {
+        return {
+            name: faker.company.companyName(),
+            img: faker.image.cats(512, 512, true)
+        }
+    })
 
-//     r.status(200).send(test)
-// })
-const jobs = [ 'UI Artist', 'Applications Development', 'Software Development' ]
+    r.status(200).send(test)
+})
+const job_titles = [ 'UI Artist', 'Applications Development', 'Software Development' ]
 app.get('/companies/get-jobs', jsonParser, async (req, r) => {
-    const name = faker.business.name
+    const name = faker.company.companyName()
     const img = faker.image.business(512, 512, true)
     const test = new Array(5).fill().map(x => {
         return {
