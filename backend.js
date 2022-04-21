b62ee2a443f0b737e45c2ca6dec9a76a9eae17c0
Berke <mgureltol@umass.edu>
2022-04-21 12:11:09 -0400

    solving problems...

--- backend.js
+++ backend.js
@@ -111,7 +111,6 @@ app.get('/companies/get-trending-companies', jsonParser, async (req, r) => {
 
 app.get('/companies/get-companies', jsonParser, async (req, r) => {
     
-<<<<<<< HEAD
 //     const test = new Array(5).fill().map(x => {
 //         return {
 //             name: faker.company.companyName(),
@@ -122,18 +121,6 @@ app.get('/companies/get-companies', jsonParser, async (req, r) => {
 //     r.status(200).send(test)
 // })
 const job_titles = [ 'UI Artist', 'Applications Development', 'Software Development' ];
-=======
-    const test = new Array(5).fill().map(x => {
-        return {
-            name: faker.company.companyName(),
-            img: faker.image.cats(512, 512, true)
-        }
-    })
-
-    r.status(200).send(test)
-})
-const job_titles = [ 'UI Artist', 'Applications Development', 'Software Development' ]
->>>>>>> 43b0599954e5f57eed50f2d1054e31e4e5bbeb5a
 app.get('/companies/get-jobs', jsonParser, async (req, r) => {
     const name = faker.company.companyName()
     const img = faker.image.business(512, 512, true)
