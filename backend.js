9860ccc7a0c16516a232e103af3683ca085e4db5
Hue Truong <httruong@umass.edu>
2022-05-07 13:51:07 -0400

    backend-db connection

--- backend.js
+++ backend.js
@@ -10,7 +10,7 @@ import { faker } from '@faker-js/faker';
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-      rejectUnauthorized: false
+        rejectUnauthorized: false
     }
 });
 
@@ -35,12 +35,16 @@ app.use(express.static('./frontend'));
 // Query jobs from specific company in Jobs table
 app.get('/companies/company/get-jobs', async (req, r) => {
     const COMMAND = `SELECT title, descrip FROM "lancer-data".jobs, "lancer-data".companies WHERE jobs.cid = companies.loginid AND companies.cname = '${req.query.company}'`
-    client.connect();
-    client.query(COMMAND, (err, res) => {
-        if (err) { r.status(501).send("ERROR: Could not get jobs!"); }
-        else { r.status(200).send(res); }
-    });
-    client.end();
+    try {
+        const client = await pool.connect();
+        const result = await client.query(COMMAND);
+        const results = { 'results': (result) ? result.rows : null };
+        res.render('pages/db', results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
 })
 
 // Query information on company from Companies table
@@ -56,14 +60,17 @@ app.get('/companies/get-company', async (req, r) => {
 
 // Query user information from Users table
 app.get('/users/get-user', async (req, r) => {
-    const client = await pool.connect();
     const COMMAND = `SELECT * FROM users WHERE email = ${req.query.email}`
-    client.connect();
-    client.query(COMMAND, (err, res) => {
-        if (err) { r.status(501).send("ERROR: Could not get user!"); }
-        else { r.status(200).send(res); }
-    })
-    client.end();
+    try {
+        const client = await pool.connect();
+        const result = await client.query(COMMAND);
+        const results = { 'results': (result) ? result.rows : null };
+        res.render('pages/db', results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
 })
 
 // Query random top picks for home page
@@ -115,7 +122,7 @@ app.get('/companies/get-trending-companies', jsonParser, async (req, r) => {
 })
 
 app.get('/companies/get-companies', jsonParser, async (req, r) => {
-    
+
     const test = new Array(5).fill().map(x => {
         return {
             name: faker.company.companyName(),
@@ -125,7 +132,7 @@ app.get('/companies/get-companies', jsonParser, async (req, r) => {
 
     r.status(200).send(test)
 })
-const job_titles = [ 'UI Artist', 'Applications Development', 'Software Development' ];
+const job_titles = ['UI Artist', 'Applications Development', 'Software Development'];
 app.get('/companies/get-jobs', jsonParser, async (req, r) => {
     const name = faker.company.companyName()
     const img = faker.image.business(512, 512, true)
@@ -143,72 +150,97 @@ app.get('/companies/get-jobs', jsonParser, async (req, r) => {
 //Takes given user data and submits to a new "applications" table
 //Sends user name, email, and job applied to
 app.post('/submitapp', jsonParser, async (req, r) => {
-    const COMMAND = 
-    `INSERT INTO "lancer-data".applications (fname, lname, email, job)
+    const COMMAND =
+        `INSERT INTO "lancer-data".applications (fname, lname, email, job)
     VALUES ('${req.query.fname}', '${req.query.lname}', '${req.query.email}', ${req.query.job})
     ON DUPLICATE KEY UPDATE
     fname = '${req.query.fname}',
     lname = '${req.query.lname}',
     job = ${req.query.job};`;
-    client.query(COMMAND, (err, res) => {
-             if(err){ r.status(501).send("ERROR: Could not submit application information!"); }
-             else{ r.status(200).send(res); }
-         });
-         client.end();
+    try {
+        const client = await pool.connect();
+        const result = await client.query(COMMAND);
+        const results = { 'results': (result) ? result.rows : null };
+        res.render('pages/db', results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
 });
 
 //Takes data from HTML form and submits info to "users" table
 //Sends user name, email, password
 app.post('/signup/user', jsonParser, async (req, r) => {
-    const COMMAND = 
-    `INSERT INTO "lancer-data".users (fname, lname, email, pass)
+    const COMMAND =
+        `INSERT INTO "lancer-data".users (fname, lname, email, pass)
     VALUES ('${req.query.fname}', '${req.query.lname}', '${req.query.email}', '${req.query.pass}')
     ON DUPLICATE KEY UPDATE
     fname = '${req.query.fname}',
     lname = '${req.query.lname}',
     pass = '${req.query.pass}';`;
-    client.query(COMMAND, (err, res) => {
-             if(err){ r.status(501).send("ERROR: Could not submit user information!"); }
-             else{ r.status(200).send(res); }
-         });
-         client.end();
+    try {
+        const client = await pool.connect();
+        const result = await client.query(COMMAND);
+        const results = { 'results': (result) ? result.rows : null };
+        res.render('pages/db', results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
 });
 
 //Takes data from HTML form and submits info to "companies" table
 //Sends company name, login id (email), password
 app.post('/signup/company', jsonParser, async (req, r) => {
-    const COMMAND = 
-    `INSERT INTO "lancer-data".companies (cname, loginid, pass)
+    const COMMAND =
+        `INSERT INTO "lancer-data".companies (cname, loginid, pass)
     VALUES ('${req.query.cname}', '${req.query.loginid}', '${req.query.pass}')
     ON DUPLICATE KEY UPDATE
     cname = '${req.query.cname}',
     pass = '${req.query.pass}';`;
-    client.query(COMMAND, (err, res) => {
-             if(err){ r.status(501).send("ERROR: Could not submit company information!"); }
-             else{ r.status(200).send(res); }
-         });
-         client.end();
+    try {
+        const client = await pool.connect();
+        const result = await client.query(COMMAND);
+        const results = { 'results': (result) ? result.rows : null };
+        res.render('pages/db', results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
 });
 
 app.get('/signin', jsonParser, async (req, r) => {
     const COMMAND = `SELECT fname, lname FROM "lancer-data".users 
     WHERE email = '${req.query.email}' AND pass = '${req.query.pass}';`;
-    client.query(COMMAND, (err, res) => {
-        if(err){ r.status(501).send("ERROR: Could not retrieve user information! Email or password is incorrect."); }
-        else{ r.status(200).send(res); }
-    });
-    client.end();
+    try {
+        const client = await pool.connect();
+        const result = await client.query(COMMAND);
+        const results = { 'results': (result) ? result.rows : null };
+        res.render('pages/db', results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
 });
 
 app.get('/company/get-applicants', jsonParser, async (req, r) => {
     const COMMAND = `SELECT fname, lname, email, job FROM "lancer-data".applications, "lancer-data".jobs, "lancer-data".companies 
     WHERE applications.job = jobs.id AND jobs.cid = companies.loginid 
     AND companies.loginid = '${req.query.loginid}';`;
-    client.query(COMMAND, (err, res) => {
-        if(err){ r.status(501).send("ERROR: Could not retrieve application information!"); }
-        else{ r.status(200).send(res); }
-    });
-    client.end();
+    try {
+        const client = await pool.connect();
+        const result = await client.query(COMMAND);
+        const results = { 'results': (result) ? result.rows : null };
+        res.render('pages/db', results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
 });
 
 app.listen(port, () => {
